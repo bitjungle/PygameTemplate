c9c7b8076b5cd8bc7929a0f6c3f3c213e5d90c41
bitjungle <devel@bitjungle.com>
2020-09-13 12:38:18 +0200

    New demo - keyboard control

---
+++ pygame-template_demo_keyboard.py
@@ -0,0 +1,83 @@
+''' Pygame template demo
+
+A pygame template to get my students quickly up and running.
+
+Copyright (C) 2020 BITJUNGLE Rune Mathisen
+This code is licensed under a GPLv3 license 
+See http://www.gnu.org/licenses/gpl-3.0.html 
+'''
+import pygame
+import sys
+import math
+import random 
+
+# -- Game classes ------------------------------------------------------
+import pygame_template_colors as color
+
+# -- Game window properties --------------------------------------------
+DISPLAY_WIDTH = 800           # pixels
+DISPLAY_HEIGHT = 600          # pixels
+WINDOW_TITLE = 'Your Title Here'
+SCREEN_BG_COLOR = color.black # From pygame_template_colors.py
+FPS = 30                      # Frames per second
+
+# -- Preparing game window ---------------------------------------------
+# https://www.pygame.org/docs/ref/display.html?highlight=init#pygame.display.init
+pygame.init() 
+# https://www.pygame.org/docs/ref/time.html#pygame.time.Clock
+clock = pygame.time.Clock() 
+# https://www.pygame.org/docs/ref/display.html#pygame.display.set_mode
+screen = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+# https://www.pygame.org/docs/ref/display.html#pygame.display.set_caption
+pygame.display.set_caption(WINDOW_TITLE)
+
+running = True # The program will run as long as this variable is true
+
+# -- Preparing game objects --------------------------------------------
+pad_width = 100 # pixels
+pad_height = 25 # pixels
+pad = pygame.Surface((pad_width, pad_height))
+pad.fill(color.tomato)
+xpos = 100 # pad start position
+ypos = DISPLAY_HEIGHT - 2*pad_height
+dx = 0
+
+while running: # Main loop
+    for event in pygame.event.get(): # https://www.pygame.org/docs/ref/event.html
+        if event.type == pygame.QUIT: 
+            running = False # Exiting the main loop
+        elif event.type == pygame.KEYDOWN:
+            key = pygame.key.get_pressed()
+            if key[pygame.K_LEFT]:
+                dx = -10
+            if key[pygame.K_RIGHT]:
+                dx = 10
+        elif event.type == pygame.KEYUP:
+            dx = 0
+        elif event.type == pygame.MOUSEMOTION:
+            mouse = pygame.mouse.get_pos()
+            print('Mouse pos: ', mouse)
+        else:
+            print('Unhandled event', event)
+
+    screen.fill(SCREEN_BG_COLOR)  # Blanking the screen
+
+    # -- Implement game code here --------------------------------------
+    xpos += dx # Move the pad
+    if xpos == 0 or xpos > DISPLAY_WIDTH - pad_width: # Stop pad at screen edge
+        dx = 0
+
+    # -- Drawing game objects ------------------------------------------
+    # screen.blit() your game objects here
+    # https://www.pygame.org/docs/ref/surface.html?highlight=blit#pygame.Surface.blit
+    screen.blit(pad, (xpos, ypos))
+
+    # Update the full display Surface to the screen
+    # https://www.pygame.org/docs/ref/display.html#pygame.display.flip
+    pygame.display.flip()
+    # https://www.pygame.org/docs/ref/time.html#pygame.time.Clock.tick
+    clock.tick(FPS)
+
+# -- Exiting... --------------------------------------------------------
+pygame.quit() # https://www.pygame.org/docs/ref/pygame.html#pygame.quit
+sys.exit()    # https://docs.python.org/3.8/library/sys.html#sys.exit
